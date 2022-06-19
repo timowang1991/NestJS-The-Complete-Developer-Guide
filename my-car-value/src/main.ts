fe1c360d26ac8c738712ef879b44b7f90d4eeead
Timo Wang <timo@steaker.com>
2022-06-19 21:26:34 +0800

    move global middleware and pipes to app.module.ts

--- my-car-value/src/main.ts
+++ my-car-value/src/main.ts
@@ -1,16 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import cookieSession from 'cookie-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({ keys: ['asdasds'] }));
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // only allow whatever is defined in the dto to pass through, additional properties are stripped out
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
--- my-car-value/test/auth.e2e-spec.ts
+++ my-car-value/test/auth.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Authentication System', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,17 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('handles a signup request', () => {
+    const requestEmail = 'email2@email.com';
+
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/auth/signup')
+      .send({ email: requestEmail, password: 'password' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(requestEmail);
+      });
   });
 });
